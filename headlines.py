fbeb7d36d3a6f6ba3eedf9291dc2e21971d5810d
Igo da Costa Andrade <igoandrade@uft.edu.br>
2022-08-05 16:33:40 -0300

    Rotas dinâmicas

--- headlines.py
+++ headlines.py
@@ -3,14 +3,22 @@ import feedparser
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {
+    'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640'
+}
 
-@app.route('/')
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+def get_feed(rss):
+    return feedparser.parse(rss)
+
+@app.route('/<rss>')
+def get_news(rss):
+    feed = get_feed(RSS_FEEDS[rss])
 
     feeds = feed['entries']
-    return render_template("index.html", feeds=feeds)
+    return render_template("index.html", rss=rss, feeds=feeds)
 
 if __name__=='__main__':
     app.run(debug=True, port=5000)
